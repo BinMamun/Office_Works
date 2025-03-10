b284e72cd177398e7b95ec52453c5f5330296f7a
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-10 11:01:08 +0600

    Configure file and database logger in the application

--- src/WingtipToys/WingtipToys/Global.asax.cs
+++ src/WingtipToys/WingtipToys/Global.asax.cs
@@ -1,21 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
+using System.IO;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace WingtipToys
 {
-    public class Global : HttpApplication
+	public class Global : HttpApplication
     {
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+		{
+            Exception ex = Server.GetLastError();
+
+            var fileName = $"log_{DateTime.Now:yyyy-MM-dd}.txt";
+            var filePath = Server.MapPath($"~/App_Data/{fileName}");
+            var errorMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Error: {ex.Message}\nStack Trace: {ex.StackTrace}\n\n";
+
+            File.AppendAllText(filePath, errorMessage);
+
+            var connString = "Server=SANON;Database=WingtipToysDb; Trusted_Connection=True; TrustServerCertificate=True";
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "INSERT INTO ErrorLogs (ErrorMessage, StackTrace) VALUES (@ErrorMessage, @StackTrace)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ErrorMessage", ex.Message);
+                    cmd.Parameters.AddWithValue("@StackTrace", ex.StackTrace);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Server.ClearError();
         }
     }
 }
