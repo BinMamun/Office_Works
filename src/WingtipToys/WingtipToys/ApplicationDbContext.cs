14a203ae7d3f2eb73a77d927fd8c700572cd330f
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-10 14:21:06 +0600

    add ApplicationDbContext class

---
+++ src/WingtipToys/WingtipToys/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
