59953ebcaf893cf9be4e97be20b3bf2db23a8961
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-11 14:39:55 +0600

    Show Products List

---
+++ src/WingtipToys/WingtipToys/Pages/ProductList.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using WingtipToys.Models;
+
+namespace WingtipToys.Pages
+{
+	public partial class ProductList : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new ApplicationDbContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
+        }
+    }
+}
