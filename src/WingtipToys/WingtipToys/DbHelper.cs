04f13cab7ff9048455f38905f0472f9961aca3ac
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-11 15:41:01 +0600

    Create DbHelper Statice class to manage SqlConnection and data fetching Methods

---
+++ src/WingtipToys/WingtipToys/DbHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WingtipToys
+{
+	public static class DbHelper
+	{
+		public static string _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+		public static SqlDataReader ExecuteProcedureReader(string procedureName, params SqlParameter[] parameters)
+		{
+			SqlConnection conn = new SqlConnection(_connectionString);
+			SqlCommand cmd = new SqlCommand(procedureName, conn)
+			{
+				CommandType = CommandType.StoredProcedure
+			};
+
+			if (parameters != null)
+			{
+				cmd.Parameters.AddRange(parameters);
+			}
+
+			conn.Open();
+			return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+		}
+
+
+		public static int ExecuteProcedureNonQuery(string procedureName, params SqlParameter[] parameters)
+		{
+			using (SqlConnection conn = new SqlConnection(_connectionString))
+			{
+				using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					if (parameters != null)
+					{
+						cmd.Parameters.AddRange(parameters);
+					}
+
+					conn.Open();
+					return cmd.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public static object ExecuteProcedureScalar(string procedureName, params SqlParameter[] parameters)
+		{
+			using (SqlConnection conn = new SqlConnection(_connectionString))
+			{
+				using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					if (parameters != null)
+					{
+						cmd.Parameters.AddRange(parameters);
+					}
+
+					conn.Open();
+					return cmd.ExecuteScalar();
+				}
+			}
+		}
+	}
+}
