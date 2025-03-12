a3b518d53dc6ea6542847b4692f3b98793c5fc7e
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-12 10:44:35 +0600

    write GetProducts method to get products using ado.net

--- src/WingtipToys/WingtipToys/Pages/ProductList.aspx.cs
+++ src/WingtipToys/WingtipToys/Pages/ProductList.aspx.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.ModelBinding;
 using System.Web.UI;
@@ -12,16 +15,41 @@ namespace WingtipToys.Pages
 		{
 
 		}
+		//public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+		//{
+		//	var _db = new ApplicationDbContext();
+		//	IQueryable<Product> query = _db.Products;
+		//	if (categoryId.HasValue && categoryId > 0)
+		//	{
+		//		query = query.Where(p => p.CategoryID == categoryId);
+		//	}
+		//	return query;
+		//}
+		public IList<Product> GetProducts([QueryString("id")] int? categoryId)
+		{
+			List<Product> products = new List<Product>();
 
-        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
-        {
-            var _db = new ApplicationDbContext();
-            IQueryable<Product> query = _db.Products;
-            if (categoryId.HasValue && categoryId > 0)
-            {
-                query = query.Where(p => p.CategoryID == categoryId);
-            }
-            return query;
-        }
-    }
+			SqlParameter catId = new SqlParameter("@CategoryId", SqlDbType.Int)
+			{
+				Value = categoryId
+			};
+
+			using (SqlDataReader reader = DbHelper.ExecuteProcedureReader("GetProductsByCategory", catId))
+			{
+				while (reader.Read())
+				{
+					products.Add(new Product
+					{
+						ProductID = reader.GetInt32(0),
+						ProductName = reader.GetString(1),
+						ImagePath = reader.GetString(4),
+						UnitPrice = reader.GetDouble(2),
+						Description = reader.GetString(3),
+						CategoryID = reader.GetInt32(5)
+					});
+				}
+			}
+			return products;
+		}
+	}
 }
