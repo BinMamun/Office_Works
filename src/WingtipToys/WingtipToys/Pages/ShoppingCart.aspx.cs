29b921208d421727472b9d0fa083b8e1e49b0200
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-12 16:25:20 +0600

    Add multiple methods in ShoppingCartActions Class to be able to update Cart

--- src/WingtipToys/WingtipToys/Pages/ShoppingCart.aspx.cs
+++ src/WingtipToys/WingtipToys/Pages/ShoppingCart.aspx.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingtipToys.Logic;
 using WingtipToys.Models;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Web.ModelBinding;
 
 namespace WingtipToys.Pages
 {
@@ -29,6 +29,7 @@ namespace WingtipToys.Pages
                     LabelTotalQtyText.Text = "";
                     lblTotalQty.Text = "";
                     ShoppingCartTitle.InnerText = "Shopping Cart is Empty";
+                    UpdateBtn.Visible = false;
                 }
             }
         }
@@ -38,9 +39,52 @@ namespace WingtipToys.Pages
 			return actions.GetCartItems();
 		}
 
-		protected void CartList_SelectedIndexChanged(object sender, EventArgs e)
-		{
+        public List<CartItem> UpdateCartItems()
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                String cartId = usersShoppingCart.GetCartId();
 
-		}
-	}
+                ShoppingCartActions.ShoppingCartUpdates[] cartUpdates = new ShoppingCartActions.ShoppingCartUpdates[CartList.Rows.Count];
+
+                for (int i = 0; i < CartList.Rows.Count; i++)
+                {
+                    IOrderedDictionary rowValues = new OrderedDictionary();
+                    rowValues = GetValues(CartList.Rows[i]);
+                    cartUpdates[i].ProductId = Convert.ToInt32(rowValues["ProductID"]);
+
+                    CheckBox cbRemove = new CheckBox();
+                    cbRemove = (CheckBox)CartList.Rows[i].FindControl("Remove");
+                    cartUpdates[i].RemoveItem = cbRemove.Checked;
+
+                    TextBox quantityTextBox = new TextBox();
+                    quantityTextBox = (TextBox)CartList.Rows[i].FindControl("PurchaseQuantity");
+                    cartUpdates[i].PurchaseQuantity = Convert.ToInt16(quantityTextBox.Text.ToString());
+                }
+                usersShoppingCart.UpdateShoppingCartDatabase(cartId, cartUpdates);
+                CartList.DataBind();
+                lblTotal.Text = String.Format("{0:c}", usersShoppingCart.GetTotal());
+                return usersShoppingCart.GetCartItems();
+            }
+        }
+
+        public static IOrderedDictionary GetValues(GridViewRow row)
+        {
+            IOrderedDictionary values = new OrderedDictionary();
+            foreach (DataControlFieldCell cell in row.Cells)
+            {
+                if (cell.Visible)
+                {
+                    // Extract values from the cell.
+                    cell.ContainingField.ExtractValuesFromCell(values, cell, row.RowState, true);
+                }
+            }
+            return values;
+        }
+
+        protected void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            UpdateCartItems();
+        }
+    }
 }
