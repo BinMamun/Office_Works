82b394f6694874500ce765dfae55f14b52697237
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-12 12:27:17 +0600

    Add AddToCart page

---
+++ src/WingtipToys/WingtipToys/Pages/AddToCart.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.UI;
+using WingtipToys.Logic;
+
+namespace WingtipToys.Pages
+{
+	public partial class AddToCart : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            string rawId = Request.QueryString["ProductID"];
+            if (!string.IsNullOrEmpty(rawId) && int.TryParse(rawId, out int productId))
+            {
+                using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+                {
+					usersShoppingCart.AddToCart(productId);
+                }
+            }
+            else
+            {
+                throw new Exception("ERROR : It is illegal to load AddToCart.aspx without setting a ProductId.");
+            }
+            Response.Redirect("ShoppingCart.aspx");
+        }
+	}
+}
