1ef32aa059dbe6985a761953f5fba5432f9c8761
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-13 14:37:33 +0600

    Write GetCategories method to get all categories

---
+++ src/ProductManager/ProductManager/Logic/CategoryLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ProductManager.Logic
+{
+	public class CategoryLogic
+	{
+		public static DataTable GetCategories()
+		{
+			return DbUtility.ExecuteReader("GetCategories");
+		}
+	}
+}
