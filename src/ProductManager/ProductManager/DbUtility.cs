8023563df5104a7dac706d1f5a0755cd16d2e1fa
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-13 11:53:58 +0600

    Add ExecuteStoredProcedure and ExecuteReader Generic method

--- src/ProductManager/ProductManager/DbUtility.cs
+++ src/ProductManager/ProductManager/DbUtility.cs
@@ -1,8 +1,11 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
-public static class DbUtility
+public class DbUtility
 {
     private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["ProductDB"].ConnectionString;
 
@@ -24,6 +27,15 @@ public static class DbUtility
         }
     }
 
+    public static void ExecuteStoredProcedure(string procedureName, Dictionary<string, object> parameters)
+    {
+        SqlParameter[] sqlParameters = parameters?
+            .Select(p => new SqlParameter(p.Key, p.Value ?? DBNull.Value))
+            .ToArray() ?? Array.Empty<SqlParameter>();
+
+        ExecuteNonQuery(procedureName, sqlParameters);
+    }
+
     // Execute Reader (SELECT)
     public static DataTable ExecuteReader(string procedureName, params SqlParameter[] parameters)
     {
@@ -36,6 +48,32 @@ public static class DbUtility
                 {
                     cmd.Parameters.AddRange(parameters);
                 }
+                conn.Open();
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+    }
+
+    public static DataTable ExecuteReader(string procedureName, Dictionary<string, object> parameters = null)
+    {
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand(procedureName, conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters
+                        .Select(p => new SqlParameter(p.Key, p.Value ?? DBNull.Value))
+                        .ToArray());
+                }
+
                 conn.Open();
                 using (SqlDataAdapter da = new SqlDataAdapter(cmd))
                 {
