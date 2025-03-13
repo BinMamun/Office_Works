a1249141919a5c28bbf4b6d0ddcaf0a48403e5cc
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-13 11:56:05 +0600

    Write GetProducts and GetProductsByCategory method in ProductLogic class

---
+++ src/ProductManager/ProductManager/Logic/ProductLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProductManager.Logic
+{
+	public class ProductLogic
+	{
+		public static DataTable GetProducts()
+		{
+			return DbUtility.ExecuteReader("GetProducts");
+		}
+
+		public static DataTable GetProductsByCategory(int categoryId)
+		{
+			return DbUtility.ExecuteReader("GetProductsByCategory",
+				new Dictionary<string, object>
+				{
+					{"@CategoryId", categoryId}
+				});
+		}
+	}
+}
