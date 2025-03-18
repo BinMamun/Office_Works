699214a86da673d2e259b2ada1872bda279d28c0
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-18 10:51:22 +0600

    Create ProductReportModel

---
+++ src/ProductManager/ProductManager/Model/ProductReportModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ProductManager.Reports
+{
+    public class ProductReportModel
+    {
+        public string PicturePath { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }   
+}
