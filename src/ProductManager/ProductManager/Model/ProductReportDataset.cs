6e026b4ffd6fb595a7169249053bebe0a8ffc171
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-18 11:26:24 +0600

    Create ReportingServices project and Create ProductReportModel

---
+++ src/ProductManager/ProductManager/Model/ProductReportDataset.cs
@@ -0,0 +1,13 @@
+﻿using ReportingServices.Model;
+using System.Collections.Generic;
+
+namespace ProductManager.Model
+{
+	public class ProductReportDataset
+	{
+        public static List<ProductReportModel> GetProductReportData()
+        {
+            return DbUtility.ExecuteReportReader<ProductReportModel>("GetProducts");
+        }
+    }
+}
---
+++ src/ProductManager/ReportingServices/Model/ProductReportModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportingServices.Model
+{
+	public class ProductReportModel
+	{
+		public string PicturePath { get; set; }
+		public string ProductName { get; set; }
+		public string CategoryName { get; set; }
+		public decimal Price { get; set; }
+		public int Stock { get; set; }
+	}
+}
