4ee9a2b83a4a311a8a71bb4601617cedcb3608aa
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-18 15:23:07 +0600

    Install Winform nuget

--- src/ProductManager/ProductManager/Pages/Products.aspx.designer.cs
+++ src/ProductManager/ProductManager/Pages/Products.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace ProductManager.Pages
 		protected global::System.Web.UI.WebControls.Button btnAddProduct;
 
 		/// <summary>
+		/// btnShowReport control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnShowReport;
+
+		/// <summary>
 		/// gvProducts control.
 		/// </summary>
 		/// <remarks>
