8e8cd04b334d4e54d7df13bf5ea3bfca29129d4a
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-05 18:38:01 +0600

    Create StudentRepository  and use repository methods

---
+++ src/SchoolManagement/SchoolManagement.Domain/RepositoryContracts/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagement.Domain.Entities;
+
+namespace SchoolManagement.Domain.RepositoryContracts
+{
+    public interface IStudentRepository : IRepository<Student, Guid>
+    {
+        Task<Student> GetStudentById(Guid studentId);
+        Task<IEnumerable<Student>> GetAllStudents();
+        Task AddStudent(Student student);
+        Task UpdateStudent(Student student);
+        Task DeleteStudent(Guid studentId);
+    }
+}
---
+++ src/SchoolManagement/SchoolManagement.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,36 @@
+﻿using SchoolManagement.Domain;
+using SchoolManagement.Domain.Entities;
+using SchoolManagement.Domain.RepositoryContracts;
+
+namespace SchoolManagement.Infrastructure.Repositories
+{
+    public class StudentRepository : Repository<Student, Guid>, IStudentRepository
+    {
+        public StudentRepository(ISqlUtility sqlUtility) : base(sqlUtility)
+        {
+        }
+
+        public async Task<Student> GetStudentById(Guid studentId)
+        {
+            return await GetByIdAsync(studentId);
+        }
+
+        public async Task<IEnumerable<Student>> GetAllStudents()
+        {
+            return await GetAllAsync();
+        }
+
+        public async Task AddStudent(Student student)
+        {
+            await AddAsync(student);
+        }
+        public async Task UpdateStudent(Student student)
+        {
+            await UpdateAsync(student);
+        }
+        public async Task DeleteStudent(Guid studentId)
+        {
+            await DeleteAsync(studentId);
+        }
+    }
+}
