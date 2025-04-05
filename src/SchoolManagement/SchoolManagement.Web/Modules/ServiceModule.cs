20329f4b85c16b5d73dc887accbb944dbcdee54c
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-05 17:11:03 +0600

    Register UnitOfWork and SchoolManagemntUnitOfWork for DI

--- src/SchoolManagement/SchoolManagement.Web/Modules/ServiceModule.cs
+++ src/SchoolManagement/SchoolManagement.Web/Modules/ServiceModule.cs
@@ -1,10 +1,14 @@
-﻿namespace SchoolManagement.Web.Modules
+﻿using SchoolManagement.Domain;
+using SchoolManagement.Infrastructure.UnitOfWork;
+
+namespace SchoolManagement.Web.Modules
 {
     public static class ServiceModule
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ISchoolManagementUnitOfWork, SchoolManagemntUnitOfWork>();
         }
     }
 }
