5682c2fd953f339e26ecd2cc5acc167e55ef5270
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-06 23:58:40 +0600

    create the StudentManagementService Class without using UnitOfWork Pattern

--- src/SchoolManagement/SchoolManagement.Application/Services/StudentManagementService.cs
+++ src/SchoolManagement/SchoolManagement.Application/Services/StudentManagementService.cs
@@ -1,15 +1,41 @@
-﻿using SchoolManagement.Domain;
-using SchoolManagement.Domain.Entities;
+﻿using SchoolManagement.Domain.Entities;
+using SchoolManagement.Domain.RepositoryContracts;
 using SchoolManagement.Domain.ServiceContracts;
 
 namespace SchoolManagement.Application.Services
 {
     public class StudentManagementService : IStudentManagementService
     {
-        
-        public StudentManagementService()
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentManagementService(IStudentRepository studentRepository)
         {
+            _studentRepository = studentRepository;
         }
-        
+
+        public async Task<Student> GetStudentByIdAsync(Guid id)
+        {
+            return await _studentRepository.GetByIdAsync(id, "sp_GetStudentById");
+        }
+
+        public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            return await _studentRepository.GetAllAsync("sp_GetAllStudents");
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            await _studentRepository.AddAsync(student, "sp_AddStudent");
+        }
+
+        public async Task UpdateStudentAsync(Student student)
+        {
+            await _studentRepository.UpdateAsync(student, "sp_UpdateStudent");
+        }
+
+        public async Task DeleteStudentAsync(Guid id)
+        {
+            await _studentRepository.DeleteAsync(id, "sp_DeleteStudent");
+        }
     }
 }
