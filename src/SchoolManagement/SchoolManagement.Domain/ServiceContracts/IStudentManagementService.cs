002e61a15a51bda2d0a7d0dde089101aff4a88f2
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-07 00:25:24 +0600

    Write Get all Students controller and View

--- src/SchoolManagement/SchoolManagement.Domain/ServiceContracts/IStudentManagementService.cs
+++ src/SchoolManagement/SchoolManagement.Domain/ServiceContracts/IStudentManagementService.cs
@@ -1,6 +1,13 @@
-﻿namespace SchoolManagement.Domain.ServiceContracts
+﻿using SchoolManagement.Domain.Entities;
+
+namespace SchoolManagement.Domain.ServiceContracts
 {
     public interface IStudentManagementService
     {
+        Task<Student> GetStudentByIdAsync(Guid id);
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+        Task AddStudentAsync(Student student);
+        Task UpdateStudentAsync(Student student);
+        Task DeleteStudentAsync(Guid id);
     }
 }
---
+++ src/SchoolManagement/SchoolManagement.Web/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Domain.ServiceContracts;
+
+namespace SchoolManagement.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentManagementService _studentManagementService;
+
+        public StudentController(IStudentManagementService studentManagementService)
+        {
+            _studentManagementService = studentManagementService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _studentManagementService.GetAllStudentsAsync();
+            return View(students);
+        }
+    }
+}
