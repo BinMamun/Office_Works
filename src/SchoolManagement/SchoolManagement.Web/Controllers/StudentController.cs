eff10376b71de33185da7df44a632c010a7ecf8c
Abdullah Bin Mamun <binbean44@gmail.com>
2025-04-07 00:27:56 +0600

    add Student Create, Edit and Delete Controller methods

--- src/SchoolManagement/SchoolManagement.Web/Controllers/StudentController.cs
+++ src/SchoolManagement/SchoolManagement.Web/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Domain.Entities;
 using SchoolManagement.Domain.ServiceContracts;
 
 namespace SchoolManagement.Web.Controllers
@@ -16,6 +17,58 @@ namespace SchoolManagement.Web.Controllers
         {
             var students = await _studentManagementService.GetAllStudentsAsync();
             return View(students);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await _studentManagementService.AddStudentAsync(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await _studentManagementService.GetStudentByIdAsync(id);
+            if (student == null) return NotFound();
+
+            return View(student);
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await _studentManagementService.UpdateStudentAsync(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var student = await _studentManagementService.GetStudentByIdAsync(id);
+            if (student == null) return NotFound();
+
+            return View(student);
+        }
+
+        // POST: /Student/DeleteConfirmed
+        [HttpPost, ValidateAntiForgeryToken, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _studentManagementService.DeleteStudentAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
